2a7de9abc0cb4b34d5372fdc2beade8775c8977e
joaoptgaino <joaoptgaino@hotmail.com>
2020-12-03 09:54:49 -0300

    Adding tbe controller to the route, since I forgot about that little detail

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
 import PostsController from './Controllers/PostsController';
+import EmailController from './Controllers/EmailController';
 
 const routes = express.Router();
 const postsController = new PostsController;
+const emailController = new EmailController;
 routes.get('/',(req:Request,res:Response)=>{
     res.json({
         message:"E aí"
@@ -11,6 +13,6 @@ routes.get('/',(req:Request,res:Response)=>{
 
 routes.get('/posts',postsController.index);
 routes.post('/posts',postsController.create);
-
+routes.post('/mailer',emailController.enviar);
 
 export default routes;
